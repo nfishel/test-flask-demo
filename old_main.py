6d7a4c1bfd7818b602108bb877f306b13639296c
Fishel <nfishel@hse.k12.in.us>
2023-02-25 21:19:32 +0000

    removed session

--- old_main.py
+++ old_main.py
@@ -1,16 +1,8 @@
-from flask import Flask, request, render_template, redirect, url_for, session, jsonify
-from flask_session import Session
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from random import randint, choice
 from cs50 import SQL
 
 app = Flask(__name__)
-
-
-#Session Config
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = 'filesystem'
-
-Session(app)
 
 
 db = SQL("sqlite:///votes.db")
